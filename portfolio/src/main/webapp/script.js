d54961a34a51255ba57545c8e7fd39fc889f104b
Prabhjot Grewal <prabsg@google.com>
2020-07-17 08:34:07 +0000

    Add Google Maps integration for location on each image in gallery, with corresponding styling and callback function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,13 +35,13 @@ function openSimpleImageModal(modalId, modalImgId, captionId, closeId, imgSrc, c
   const modalImg = document.getElementById(modalImgId);
   const caption = document.getElementById(captionId);
 
-  modal.style.display = "block";
+  modal.style.display = 'block';
   modalImg.src = imgSrc;
   caption.innerText = captionText;
 
   const closeBtn = document.getElementById(closeId);
   closeBtn.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   };
 }
 
@@ -51,14 +51,15 @@ function openSimpleImageModal(modalId, modalImgId, captionId, closeId, imgSrc, c
 function advanceSlides(n) {
   currIndex += n;
   currIndex = showSlidePicture(currIndex);
+  initMap();
 };
 
 /**
  * Show the image from the gallery at the given position.
  */
 function showSlidePicture(n) {
-  const slides = document.getElementsByClassName("slide-pic");
-  const caption = document.getElementById("slide-caption");
+  const slides = document.getElementsByClassName('slide-pic');
+  const caption = document.getElementById('slide-caption');
 
   // If not on pics.html document wil not contain any slide elements.
   if (slides.length === 0) {
@@ -74,12 +75,12 @@ function showSlidePicture(n) {
   }
   
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
 
-  slides[newIndex].style.display = "block";
-  slides[newIndex].getElementsByClassName("slide-counter")[0].innerText =
-     (newIndex + 1).toString() + "/" + slides.length.toString();
+  slides[newIndex].style.display = 'block';
+  slides[newIndex].getElementsByClassName('slide-counter')[0].innerText =
+     (newIndex + 1).toString() + '/' + slides.length.toString();
   caption.innerText = picCaptions[newIndex];
 
   return newIndex;
@@ -89,19 +90,35 @@ function showSlidePicture(n) {
  * Initialise map for currently displayed image.
  */
 function initMap() {
+  let map = new google.maps.Map(
+    document.getElementById('map'),
+    {zoom: picZoom[currIndex], center: picLocations[currIndex]}
+  )
 
+  let marker = new google.maps.Marker(
+    {position: picLocations[currIndex], map: map}
+  );
 }
 
-let picCaptions = [
-  "This was taken outside King's College Chapel in Cambridge last summer. I \
-    decided to take a scenic route and accidentally timed this photo with a \
-    cyclist going past.",
-  "IC A vs Birmingham B in the NHSF National Kabaddi Tournament earlier this \
-    year. I joined Kabaddi at Imperial this year and I am loving it!",
-  "A photo of the Marina Bay Sands Hotel in Singapore during one of its light \
-    shows, taken from across the bay.",
-  "A firebreather in the desert. Taken during a desert safari and dinner \
-    excursion when visiting Dubai.",
-  "A close-up of a flower in the Flower Dome of the Gardens By the Bay in \
-    Singapore."]
+const picCaptions = [
+  `This was taken outside King's College Chapel in Cambridge last summer. I ` +
+    'decided to take a scenic route and accidentally timed this photo with ' +
+    'a cyclist going past.',
+  'IC A vs Birmingham B in the NHSF National Kabaddi Tournament earlier ' +
+    'this year. I joined Kabaddi at Imperial this year and I am loving it!',
+  'A photo of the Marina Bay Sands Hotel in Singapore during one of its '+
+    'light shows, taken from across the bay.',
+  'A firebreather in the desert. Taken during a desert safari and dinner ' +
+    'excursion when visiting Dubai.',
+  'A close-up of a flower in the Flower Dome of the Gardens By the Bay in ' +
+    'Singapore.'];
+const picLocations = [
+  {lat: 52.204, lng: 0.117},
+  {lat: 52.546, lng: -2.052},
+  {lat: 1.285, lng: 103.854},
+  {lat: 25.179, lng: 55.299},
+  {lat: 1.284, lng: 103.865}
+];
+const picZoom = [12, 12, 12, 10, 12]
+
 let currIndex = showSlidePicture(0);
