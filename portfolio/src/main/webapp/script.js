5542edfb822ea420910ea74ffe81e709d8c7a537
Prabhjot Grewal <prabsg@google.com>
2020-07-20 12:11:06 +0000

    Reformat of picture src and caption datastructure to more easily allow for images to be added with HTML generated dynamically.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,14 +35,43 @@ function openSimpleImageModal(modalId, modalImgId, captionId, closeId, imgSrc, c
   const modalImg = document.getElementById(modalImgId);
   const caption = document.getElementById(captionId);
 
-  modal.style.display = "block";
+  modal.style.display = 'block';
   modalImg.src = imgSrc;
   caption.innerText = captionText;
 
   const closeBtn = document.getElementById(closeId);
   closeBtn.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   };
+}
+
+/**
+ * Create gallery elements and set images.
+ */
+function setGalleryImages() {
+  const slideContainer = document.getElementsByClassName('slides-container')[0];
+  const nextBtn = document.getElementsByClassName('next-slide')[0];
+  
+  // If we are on Home page gallery will not be defined.
+  if (typeof slideContainer === 'undefined') {
+    return;
+  }
+
+  for (pic of picData) {
+    const slidePic = document.createElement('div');
+    slidePic.className = 'slide-pic';
+
+    const counter = document.createElement('div');
+    counter.className = 'slide-counter';
+
+    const img = document.createElement('img');
+    img.src = pic.imgSrc;
+
+    slidePic.appendChild(counter);
+    slidePic.appendChild(img);
+
+    slideContainer.insertBefore(slidePic, nextBtn);
+  }
 }
 
 /**
@@ -57,8 +86,8 @@ function advanceSlides(n) {
  * Show the image from the gallery at the given position.
  */
 function showSlidePicture(n) {
-  const slides = document.getElementsByClassName("slide-pic");
-  const caption = document.getElementById("slide-caption");
+  const slides = document.getElementsByClassName('slide-pic');
+  const caption = document.getElementById('slide-caption');
 
   // If not on pics.html document wil not contain any slide elements.
   if (slides.length === 0) {
@@ -68,13 +97,14 @@ function showSlidePicture(n) {
   let newIndex = circularIndex(n, slides);
   
   for (let slide of slides) {
-    slide.style.display = "none";
+    slide.style.display = 'none';
   }
 
-  slides[newIndex].style.display = "block";
-  slides[newIndex].getElementsByClassName("slide-counter")[0].innerText =
+  slides[newIndex].style.display = 'block';
+  
+  caption.innerText = picData[newIndex].caption;
+  slides[newIndex].getElementsByClassName('slide-counter')[0].innerText =
      (newIndex + 1).toString() + "/" + slides.length.toString();
-  caption.innerText = picCaptions[newIndex];
 
   return newIndex;
 };
@@ -97,16 +127,35 @@ function initMap() {
 
 }
 
-let picCaptions = [
-  "This was taken outside King's College Chapel in Cambridge last summer. I \
-    decided to take a scenic route and accidentally timed this photo with a \
-    cyclist going past.",
-  "IC A vs Birmingham B in the NHSF National Kabaddi Tournament earlier this \
-    year. I joined Kabaddi at Imperial this year and I am loving it!",
-  "A photo of the Marina Bay Sands Hotel in Singapore during one of its light \
-    shows, taken from across the bay.",
-  "A firebreather in the desert. Taken during a desert safari and dinner \
-    excursion when visiting Dubai.",
-  "A close-up of a flower in the Flower Dome of the Gardens By the Bay in \
-    Singapore."]
+let picData = [
+  {
+    imgSrc: './images/gallery_1.jpeg',
+    caption: 'This was taken outside King\'s College Chapel in Cambridge ' +
+      'last summer. I decided to take a scenic route and accidentally timed ' +
+      'this photo with a cyclist going past.'
+  },
+  {
+    imgSrc: './images/gallery_2.jpeg',
+    caption: 'IC A vs Birmingham B in the NHSF National Kabaddi Tournament ' +
+      'earlier this year. I joined Kabaddi at Imperial this year and I am ' +
+      'loving it!'
+  },
+  {
+    imgSrc: './images/gallery_3.jpeg',
+    caption: 'A photo of the Marina Bay Sands Hotel in Singapore during one ' +
+      'of its light shows, taken from across the bay.'
+  },
+  {
+    imgSrc: './images/gallery_4.jpeg',
+    caption: 'A firebreather in the desert. Taken during a desert safari ' +
+      'and dinner excursion when visiting Dubai.'
+  },
+  {
+    imgSrc: './images/gallery_5.jpeg',
+    caption: 'A close-up of a flower in the Flower Dome of the Gardens By ' +
+      'the Bay in Singapore.'
+  }
+]
+
+setGalleryImages();
 let currIndex = showSlidePicture(0);
