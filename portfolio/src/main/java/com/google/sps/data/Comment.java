67f1480003211578732013095ed5b67f964787eb
Prabhjot Grewal <prabsg@google.com>
2020-07-21 11:30:51 +0000

    Change unchanging variables to be const wherever appropriate.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -4,7 +4,7 @@ public class Comment {
   private final String name;
   private final String comment;
 
-  public Comment (String name, String comment) {
+  public Comment(String name, String comment) {
     this.name = name;
     this.comment = comment;
   }
