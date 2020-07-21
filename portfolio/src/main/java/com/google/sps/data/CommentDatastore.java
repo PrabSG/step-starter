ba1297e015905c01baa3de3db371bed0b3da13f1
Prabhjot Grewal <prabsg@google.com>
2020-07-21 14:44:16 +0000

    Add query to datastore to retrieve and load comments on fetch from frontend.

--- portfolio/src/main/java/com/google/sps/data/CommentDatastore.java
+++ portfolio/src/main/java/com/google/sps/data/CommentDatastore.java
@@ -3,6 +3,9 @@ package com.google.sps.data;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +26,20 @@ public class CommentDatastore implements CommentStore {
 
   @Override
   public List<Comment> getComments() {
-    return new ArrayList<>();
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Comment> comments = new ArrayList<>();
+
+    for (Entity entity : results.asIterable()) {
+      Comment comment = new Comment((String) entity.getProperty("name"),
+          (String) entity.getProperty("comment"),
+          (long) entity.getProperty("timestamp"));
+
+      comments.add(comment);
+    }
+
+    return comments;
   }
 
   @Override
